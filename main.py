a9bd8ed57850b3d1d6eb3122c2dcbca05c89e8e4
quinterojoel <157430955+quinterojoel@users.noreply.github.com>
2024-09-11 14:36:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import turtle as trtl
 screen = trtl.Screen()
 screen.bgcolor("white")
 painter = trtl.Turtle()
-painter.speed("fastest")
+painter.speed(0)
 
 size = 800
-color = "white"
+color = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+index = -1
 x = -400
 y = 400
 origin = (x, y)
@@ -13,21 +14,20 @@ origin = (x, y)
 for i in range (160):
     painter.penup()
     painter.goto(origin)
-    if color == "black":
-        color = "white"
-    else:
-        color = "black"
-    x = x + 2.5
-    y = y - 2.5
+    index += 1
+    x += 2.5
+    y -= 2.5
     origin = (x, y)
-    size = size - 5
-    painter.color(color)
+    size -= 5
+    painter.color(color[index])
     painter.begin_fill()
     painter.pendown()
     for i in range(4):
         painter.forward(size)
         painter.right(90)
     painter.end_fill()
+    if index == 6:
+        index = -1
 
 painter.hideturtle()
 screen.mainloop()
